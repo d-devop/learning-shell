8badd130a919e0af2086ffb4cceae6a2f0cb0ddd
Divya <divya5guntaka@gmail.com>
2022-11-21 15:24:40 +0530

    MySQL Script

---
+++ var.sh
@@ -0,0 +1,8 @@
+## Variable - variable is assigning a name to a set of data.
+
+# Declare a variable
+#VARNAME=DATA (No Spaces)
+a=10
+
+#Acess a variable - $VARNAME - Meaning $symbol prefixing it
+echo A -$a
